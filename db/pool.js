6d656525f503585669f702a756999076c6375c3b
David Ruttinger <192172161+druttinger@users.noreply.github.com>
2025-05-10 14:37:22 -0600

    Update .env files to work with remote server

--- db/pool.js
+++ db/pool.js
@@ -2,10 +2,14 @@ const { Pool } = require("pg");
 
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
-module.exports = new Pool({
-  host: "localhost", // or wherever the db is hosted
-  user: process.env.PGNAME,
-  database: "top_users",
-  password: process.env.PGPASSWORD, // The password for the database
-  port: process.env.PGPORT, // The default port
-});
+module.exports = new Pool(
+  process.env.DATABASE_URL
+    ? { connectionString: process.env.DATABASE_URL }
+    : {
+        host: process.env.HOST, // or wherever the db is hosted
+        user: process.env.PGNAME,
+        database: process.env.DATABASE, // The name of the database
+        password: process.env.PGPASSWORD, // The password for the database
+        port: process.env.PGPORT, // The default port
+      }
+);
